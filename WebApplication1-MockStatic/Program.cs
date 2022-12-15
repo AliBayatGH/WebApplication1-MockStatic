da66d8bd37a39220e9f65deb82ef3350ccfb508b
Ali Bayat <ali.bayat.gh@gmail.com>
2022-12-15 10:11:37 +0330

    Add project files.

---
+++ WebApplication1-MockStatic/Program.cs
@@ -0,0 +1,62 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            MyMath.Add(1, 2),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public static class MyMath
+{
+    public static Func<int, int, int> Add = (a, b) =>
+    {
+        return a + b;
+    };
+}
+
+public class Service1
+{
+    public int SomeMethod()
+    {
+        var sum = MyMath.Add(1, 4);
+
+        var result = sum * 2;
+
+        return result;
+    }
+}
---
+++ WebApplication1-MockStaticTests/MyMathTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests;
+
+public class MyMathTests
+{
+    [Fact()]
+    public void AddTest()
+    {
+        MyMath.Add = (x, y) => { return 10; };
+
+        var sut = new Service1();
+
+        var result=sut.SomeMethod();
+
+        Assert.Equal(20, result);
+    }
+}
